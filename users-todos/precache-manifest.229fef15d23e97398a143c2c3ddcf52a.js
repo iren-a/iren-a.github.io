16de6a62e087f42566f916b1dc06a51a5514843b
Irina Arinushkina <ira.arinushkina@gmail.com>
2018-11-05 18:29:42 +0300

    Add files via upload

---
+++ users-todos/precache-manifest.229fef15d23e97398a143c2c3ddcf52a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b62886d9ea2f10932e8a",
+    "url": "/static/js/main.b62886d9.chunk.js"
+  },
+  {
+    "revision": "1ae9c870ec0aa1ea41e0",
+    "url": "/static/js/1.1ae9c870.chunk.js"
+  },
+  {
+    "revision": "b62886d9ea2f10932e8a",
+    "url": "/static/css/main.ba647a6c.chunk.css"
+  },
+  {
+    "revision": "9e6d513214c5d4b7497d399b4b7fdcb7",
+    "url": "/index.html"
+  }
+];
