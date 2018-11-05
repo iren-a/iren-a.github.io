74bb464e14e059488c3cc904656224efb0970b9f
Irina Arinushkina <ira.arinushkina@gmail.com>
2018-11-05 18:33:18 +0300

    Add files via upload

---
+++ users-todos/precache-manifest.586662b5f5786bf83338a0725abd9551.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7745e5d15486d840e64",
+    "url": "/users-todos/static/js/runtime~main.f7745e5d.js"
+  },
+  {
+    "revision": "b62886d9ea2f10932e8a",
+    "url": "/users-todos/static/js/main.b62886d9.chunk.js"
+  },
+  {
+    "revision": "1ae9c870ec0aa1ea41e0",
+    "url": "/users-todos/static/js/1.1ae9c870.chunk.js"
+  },
+  {
+    "revision": "b62886d9ea2f10932e8a",
+    "url": "/users-todos/static/css/main.ba647a6c.chunk.css"
+  },
+  {
+    "revision": "fb5d5900b2225153a28656853c4c8d28",
+    "url": "/users-todos/index.html"
+  }
+];
--- users-todos/service-worker.js
+++ users-todos/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.229fef15d23e97398a143c2c3ddcf52a.js"
+  "/users-todos/precache-manifest.586662b5f5786bf83338a0725abd9551.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/users-todos/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
